da341460f78bab829c1460f8d3fb712db7582dab
Renee Sajedian <rsajedian@gmail.com>
2020-06-12 13:50:26 -0400

    created .gitignore

---
+++ word-findy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  word-findy
+//
+//  Created by Renee Sajedian on 6/12/20.
+//  Copyright © 2020 Renee Sajedian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
