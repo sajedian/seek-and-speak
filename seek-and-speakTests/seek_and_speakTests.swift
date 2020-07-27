b4b3cf8cf72b21b6375c98d195ebd6cddc519ebe
Renee Sajedian <rsajedian@gmail.com>
2020-07-27 16:41:17 -0400

    added tests for trieNode

--- seek-and-speakTests/seek_and_speakTests.swift
+++ seek-and-speakTests/seek_and_speakTests.swift
@@ -58,7 +58,54 @@ class trieTests: XCTestCase {
     }
 
     func testInsert() {
-        
+        //given
+        board.letters = ["y", "u", "i", "f",
+                         "a", "h", "g", "a",
+                         "e", "e", "d", "i",
+                         "p", "r", "b", "y"]
+
+        //when
+        //insert nonsense word
+        trie.insert(word: "peayuif")
+
+        //then
+        //nonsense word should be in trie
+        let correctWords = trie.solve(board: board)
+        XCTAssertTrue(correctWords.contains("peayuif"), "word was not inserted correctly into trie")
+
+        //prefix of nonsense word should not be in trie
+        XCTAssertFalse(correctWords.contains("peayui"), "invalid prefix was asserted into trie")
+
+    }
+}
+
+class trieNodeTests: XCTestCase {
+
+    var trieNode: TrieNode!
+
+    override func setUpWithError() throws {
+        //given
+        trieNode = TrieNode()
+    }
+
+    func testTrieNodeStartsOutEmpty() {
+        //then
+        XCTAssertTrue(trieNode.children.isEmpty, "trieNode should have no children when initialized")
+    }
+
+    func testFindOrCreateChildCreatesChild() {
+        //when
+        _ = trieNode.findOrCreateChild(for: "k")
+
+        //then
+        XCTAssertNotNil(trieNode.children["k"], "trieNode did not create child properly")
+    }
+
+    func testFindOrCreateChildDoesNotCreateDuplicate() {
+        //when
+        _ = trieNode.findOrCreateChild(for: "b")
+        _ = trieNode.findOrCreateChild(for: "b")
+        XCTAssertEqual(trieNode.children.count, 1, "trieNode should not create duplicate child")
     }
 }
 
@@ -99,4 +146,8 @@ class boardTests: XCTestCase {
         XCTAssertEqual(row, 2, "row was not 2 for index 11")
         XCTAssertEqual(col, 3, "col was not 3 for index 11")
     }
+}
+
+class gameTests(){
+    
 }
